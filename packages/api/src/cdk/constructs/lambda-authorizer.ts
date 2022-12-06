a2f32f0ca2bfb739f09c400af7c40a97cfcb1e71
Sean Kleinjung <sean@thrashplay.com>
2022-12-05 22:29:50 -0600

    Require authentication when calling API [#3] (#8)
    * Proof of concept using a lambda authorizer.
    
    * Move token allow list to secure storage.

---
+++ packages/api/src/cdk/constructs/lambda-authorizer.ts
@@ -0,0 +1,9 @@
+import { Construct } from 'constructs'
+
+export class LambdaAuthorizer extends Construct {
+  constructor(scope: Construct, id: string) {
+    {
+      super(scope, id)
+    }
+  }
+}
---
+++ packages/api/src/handlers/authorizer.ts
@@ -0,0 +1,71 @@
+import middy from '@middy/core'
+import ssm from '@middy/ssm'
+import type {
+  APIGatewayTokenAuthorizerEvent,
+  APIGatewayAuthorizerResult,
+  Context,
+} from 'aws-lambda'
+
+export type AuthorizerEnvironment = {
+  VALID_TOKEN_PARAMETER: string
+}
+
+type AuthorizerContext = Context & {
+  validToken?: string
+}
+
+const getConfig = (): AuthorizerEnvironment => {
+  const { VALID_TOKEN_PARAMETER } = process.env
+  if (!VALID_TOKEN_PARAMETER) {
+    throw new Error('VALID_TOKEN_PARAMETER is required.')
+  }
+
+  return { VALID_TOKEN_PARAMETER }
+}
+
+const createResponse = (
+  allow: boolean,
+  resource: string
+): APIGatewayAuthorizerResult => ({
+  policyDocument: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: 'execute-api:Invoke',
+        Effect: allow ? 'Allow' : 'Deny',
+        Resource: resource,
+      },
+    ],
+  },
+  principalId: 'user',
+})
+
+export const handler = middy(
+  async (
+    event: APIGatewayTokenAuthorizerEvent,
+    context: AuthorizerContext
+  ): Promise<APIGatewayAuthorizerResult> => {
+    const validToken = context.validToken
+    if (validToken === undefined) {
+      console.error(
+        `SSM parameter not set: ${getConfig().VALID_TOKEN_PARAMETER}`
+      )
+      throw new Error('Configuration error')
+    }
+
+    const token = event.authorizationToken
+    switch (token) {
+      case `Bearer ${context.validToken}`:
+        return createResponse(true, event.methodArn)
+      default:
+        throw 'Invalid token'
+    }
+  }
+).use(
+  ssm({
+    fetchData: {
+      validToken: getConfig().VALID_TOKEN_PARAMETER,
+    },
+    setToContext: true,
+  })
+)
